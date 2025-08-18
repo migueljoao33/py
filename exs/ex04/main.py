4f89500e8eb8a24bce64f03c993d83deef163fb7
João Miguel <joaomiguelalbuquerque2303@gmail.com>
2025-08-18 06:08:12 -0300

    Create main.py

---
+++ exs/ex04/main.py
@@ -0,0 +1,8 @@
+n1 = int(input("diga um número "))
+n2 = int(input("diga outro número "))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print("a soma vale {} multiplicacao {} divisao {} divisao inteira {} potencia {}" .format(s, m, d, di, e))
